37aa60c11b496cb24ea863bed65165fcce689669
phanirithvij <phanirithvij20003@gmail.com>
2018-06-08 16:46:35 +0530

    allmanga.py prints all manga links

--- allmanga.py
+++ allmanga.py
@@ -1,4 +1,26 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as soup
 from requests import request
+from string import ascii_lowercase
 
-req = request("GET", "http://www.funmanga.com/manga-list/" + " '' + [a-z] ")
+
+for alphabet in (" " + ascii_lowercase):
+    #For a-z
+    req = request("GET", "http://www.funmanga.com/manga-list/" + alphabet)
+    resp = req.text
+
+    html_soup = soup(resp, "lxml")
+
+    uls = html_soup.findAll("ul", {"class":"manga-list"})
+
+    left_col = uls[0]
+    right_col = uls[1]
+
+    lis = left_col.findAll("li")
+    lis.extend(right_col.findAll("li"))
+
+    for li in lis:
+        if li.span is not None:
+            print("completed")
+        else:
+            print("ongoing")
+        print(li.a["href"].split("/")[-1])
