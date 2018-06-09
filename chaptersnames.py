908758d773c25a746fb3cc222377e5d63bb00f6d
phanirithvij <phanirithvij20003@gmail.com>
2018-06-09 16:16:24 +0530

    as efficient as possible

--- chaptersnames.py
+++ chaptersnames.py
@@ -16,31 +16,51 @@ c = conn.cursor()
 
 
 for manga_slug in allmanga:
-    req = request("GET", "http://www.funmanga.com/" + manga_slug[0])
+    c.execute("SELECT * FROM funmanga WHERE manga_slug = ?", [manga_slug[0]])
 
-    resp = req.text
+    results = c.fetchone()
 
-    html_soup = soup(resp, "lxml")
-
-    ul = html_soup.find("ul", {"class":"chapter-list"})
-
-    lis = ul.findAll("li")
-
-    for li in lis:
-        chapter_link = li.a["href"]
-        chapter_link = "/".join(chapter_link.split("/")[3:])
+    if results != [] and results is not None:
+        print(manga_slug[0])
+    else:
         
-        c.execute("SELECT * FROM funmanga WHERE fun_chapter_link = ?", [chapter_link])
-        results = c.fetchone()
-        
-        if results != None and results != []:
-            print(results,"results")
+        req = request("GET", "http://www.funmanga.com/" + manga_slug[0])
+
+        resp = req.text
+
+        html_soup = soup(resp, "lxml")
+
+        ul = html_soup.find("ul", {"class":"chapter-list"})
+
+        run = True
+
+        try:
+            lis = ul.findAll("li")
+        except Exception as e:
+            print(e)
+            run = False
+            #This happens when 404
+
+
+        if run:
+            for li in lis:
+                chapter_link = li.a["href"]
+                chapter_link = "/".join(chapter_link.split("/")[3:])
+                
+                c.execute("SELECT * FROM funmanga WHERE fun_chapter_link = ?", [chapter_link])
+                results = c.fetchone()
+                
+                if results != None and results != []:
+                    print(results,"results")
+                else:
+                    chapter_no = chapter_link.split("/")[1]
+                    chapter_name = li.a.span.text.strip()
+
+                    c.execute("INSERT INTO funmanga (chapter_name, fun_chapter_link, manga_slug, chapter_no) VALUES (?,?,?,?)", (chapter_name, chapter_link, manga_slug[0], chapter_no))
+                    print("inserted", manga_slug[0], chapter_no, chapter_name, chapter_link)
         else:
-            chapter_no = chapter_link.split("/")[1]
-            chapter_name = li.a.span.text.strip()
-
-            c.execute("INSERT INTO funmanga (chapter_name, fun_chapter_link, manga_slug, chapter_no) VALUES (?,?,?,?)", (chapter_name, chapter_link, manga_slug[0], chapter_no))
-            print("inserted", manga_slug[0], chapter_no, chapter_name, chapter_link)
+            print("Skipped", manga_slug[0])
+            #This happens when 404
     conn.commit()
 
 c.close()
